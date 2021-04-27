44cf6f1147c59348e0911b4b6cbfe091bcae3660
Douglas vinicius <douglasv.defreitas@gmail.com>
2021-04-27 02:04:57 -0300

    fix: título em FilmDetailsActivity.kt

--- app/src/main/java/com/example/exercicio/screens/FilmDetailsActivity.kt
+++ app/src/main/java/com/example/exercicio/screens/FilmDetailsActivity.kt
@@ -2,9 +2,9 @@ package com.example.exercicio.screens
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.exercicio.R
 import com.example.exercicio.models.Film
 import com.example.exercicio.params.FilmParams
-import com.example.exercicio.R
 import kotlinx.android.synthetic.main.activity_film_details.*
 
 class FilmDetailsActivity : AppCompatActivity() {
@@ -16,15 +16,19 @@ class FilmDetailsActivity : AppCompatActivity() {
         setupView()
     }
 
-    fun setupView() {
+    private fun setupView() {
         val film = intent.extras?.getSerializable(FilmParams.FILM_FILM) as? Film
 
         film?.let {
-            filmImageDet.setImageResource(film.defImage())
-            filmTitleTv.text = film.title
+            filmImageDet.setImageResource(film.retrieveEpisodeImage())
+            filmTitleTv.text = getFilmTitle(film)
             filmOpening.text = film.openingCrawl
             directorFilm.text = film.director
             producerFilm.text = film.producer
         }
+    }
+
+    private fun getFilmTitle(film: Film): String {
+        return """${film.retrieveEpisode()}: ${film.title}"""
     }
 }
