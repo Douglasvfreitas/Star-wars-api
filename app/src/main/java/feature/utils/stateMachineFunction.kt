168a442347030f4cab761b9c411d633c58630a9f
Douglas freitas <dougasv.defreitas@gmail.com>
2022-06-29 22:24:14 -0300

    chore: adjust in imports

--- app/src/main/java/feature/utils/stateMachineFunction.kt
+++ app/src/main/java/feature/utils/stateMachineFunction.kt
@@ -2,7 +2,12 @@ package feature.utils
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.flowOn
 
 fun <T> stateMachineFunction(
     dispatcher: CoroutineDispatcher = Dispatchers.Default,
